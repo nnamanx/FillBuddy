d88a0f0a54d7207694191498d41028a6408a805f
nnamanx <lkhudadatzada@gmail.com>
2024-11-01 23:20:44 +0400

    js removed

--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-    loadDataFields();
-
-    document.getElementById("save").addEventListener("click", saveProfile);
-    document.getElementById("fill-form").addEventListener("click", fillForm);
-});
+// deleted
