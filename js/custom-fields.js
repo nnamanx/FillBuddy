5261918c7ec3d3b1f2624c88be88832d5cbbb42c
nnamanx <lkhudadatzada@gmail.com>
2024-11-09 19:10:13 +0400

    custom fields js

---
+++ js/custom-fields.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const fields = ["linkedin", "github", "website"];
+  
+    fields.forEach(fieldId => {
+      const fieldElement = document.getElementById(fieldId);
+      
+      // taking from local storage
+      const savedData = localStorage.getItem(fieldId);
+      if (savedData) {
+        fieldElement.value = savedData;
+      }
+      
+      // updating local storage 
+      fieldElement.addEventListener("input", () => {
+        localStorage.setItem(fieldId, fieldElement.value);
+      });
+    });
+  });
+  
