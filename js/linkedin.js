a0792585e0f88c784393d25d56d1761df520b98d
nnamanx <lkhudadatzada@gmail.com>
2024-11-09 19:07:35 +0400

    linkedin.js

---
+++ js/linkedin.js
@@ -0,0 +1,10 @@
+(function extractLinkedInData() {
+    const linkedInData = {
+      name: "Laman Khudadatzada",
+      experience: "Backend Developer",
+      education: "Bachelor's Degree in Computer Science"
+    };
+  
+    localStorage.setItem("linkedInData", JSON.stringify(linkedInData));
+  })();
+  
